53ba6b3ef18ca112cee82d263967410200fcda9d
Catherine Looper <loopercat@gmail.com>
2017-10-03 13:44:10 -0700

    Debugged show results function

--- app.js
+++ app.js
@@ -105,98 +105,12 @@ function showResults() {
   for(var i = 0; i < Pics.all.length; i++) {
     var liEl = document.createElement('li');
     liEl.textContent = Pics.all[i].name + ' has ' + Pics.all[i].clicks + ' votes in ' + Pics.all[i].views + ' times shown.';
+    Pics.resultsList.appendChild(liEl);
   }
 }
 
 //add event listener
-Pics.section. addEventListener('click', handleClick);
+Pics.section.addEventListener('click', handleClick);
 
 //render images on page load
 randomImage();
-
-// Pics.random = function() {
-//   var randomIndex = Math.floor(Math.random() * Pics.all.length);
-//
-//   if(Pics.doNotUse.indexOf(randomIndex) !== -1) {
-//     Pics.random();
-//   }
-//
-//   Pics.workingArray.push(Pics.all[randomIndex]);
-//   Pics.doNotUse.push(randomIndex);
-//   Pics.all[randomIndex].views++;
-// };
-//
-// Pics.displayImages = function() {
-//   Pics.random();
-//   Pics.imgEl1.src = Pics.workingArray[0].filePath;
-//   Pics.random();
-//   Pics.imgEl2.src = Pics.workingArray[1].filePath;
-//   Pics.random();
-//   Pics.imgEl3.src = Pics.workingArray[2].filePath;
-//
-// };
-//
-// //event handling
-//
-// Pics.redisplayImages = function() {
-//   for(var i = 0; i < 3; i++) {
-//     Pics.workingArray.shift();
-//   }
-//   Pics.displayImages();
-// };
-//
-// Pics.clearDoNotUseArray = function() {
-//   for(var i = 0; i < 3; i++) {
-//     Pics.doNotUse.shift();
-//   }
-// };
-//
-// Pics.clickImg1 = function() {
-//   Pics.all[Pics.doNotUse[0]].clicks++;
-// };
-//
-// Pics.clickImg2 = function() {
-//   Pics.all[Pics.doNotUse[1]].clicks++;
-// };
-//
-// Pics.clickImg3 = function() {
-//   Pics.all[Pics.doNotUse[2]].clicks++;
-// };
-//
-// Pics.reLoadPage = function() {
-//   if(Pics.totalClicksCounter > 0) {
-//     redisplayImages();
-//     clearDoNotUseArray();
-//     Pics.totalClicksCounter--;
-//     return;
-//   } else {
-//     Pics.displayResults();
-//   }
-// };
-
-// Pics.imgEl1.addEventListener('click', Pics.clickImg1);
-// Pics.imgEl2.addEventListener('click', Pics.clickImg2);
-// Pics.imgEl3.addEventListener('click', Pics.clickImg3);
-// Pics.allImages.addEventListener('click', function(event) {
-//   if (event.target.id === 'one') {
-//     Pics.clickImg1();
-//   };
-//   if (event.target.id === 'two') {
-//     Pics.clickImg2();
-//   };
-//   if (event.target.id === 'three') {
-//     Pics.clickImg3();
-//   };
-// });
-//
-//
-// Pics.displayResults = function() {
-//   Pix.allImages.innerHTML = '';
-//   for(var i = 0; i < Pics.all.length; i++) {
-//     var liEl = document.createElement('li');
-//     liEl.textContent = Pics.all[i].clicks + ' votes for the ' + Pics.all[i].pic;
-//     Pics.ulEl.appendChild(liEl);
-//   }
-// };
-//
-// Pics.displayImages();
